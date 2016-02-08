2b70b56b6aaa6a52f86dbfacbe459b74e6de649d
dwehrmann <d.wehrmann@bureau-wehrmann.eu>
2016-02-08 14:32:13 +0100

    deployment

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 orion.config.add('MAIL_NAME', 'contact')
 orion.config.add('MAIL_EMAILTO', 'contact')
 orion.config.add('MAIL_EMAILFROM', 'contact')
-
 orion.config.add('KAD_APPID', 'kadira')
 orion.config.add('KAD_APISECRET', 'kadira')
